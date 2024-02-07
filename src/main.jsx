1e38e97c0ee3d137100dcee94ff577ceac5bde10
Umit  Mester <umitmester@Umits-MacBook-Pro.local>
2024-02-07 10:55:46 -0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
